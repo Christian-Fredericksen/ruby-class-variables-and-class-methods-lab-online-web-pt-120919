4f607b7729f0c9702bf558823b5354d69c1f30ec
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-08 19:49:54 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+require "pry"
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+ 
+  def initialize(name, artist, genre)
+    @@count +=1
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    
+    
+  end
+  
+  
+  
+end
